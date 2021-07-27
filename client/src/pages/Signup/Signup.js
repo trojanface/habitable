fef13633eedfca6b2a1bf27d7410f3121040415d
trojanface <trojanface@hotmail.com>
2021-07-27 20:50:53 +0930

    POST request now working with users.php

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,28 +1,55 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './style.css';
 import { ContentContext } from '../../App';
 import Button from 'react-bootstrap/Button'
 export default function Signup() {
     const {setNewContext} = useContext(ContentContext);
+
+    const [signupField , setSignupField] = useState({
+        first_name: '',
+        last_name: '',
+        username: '',
+        password: '',
+        email: ''
+        })
+
+    const inputsHandler = (e) =>{
+        setSignupField( {...signupField, [e.target.name]: e.target.value} )
+    }
+
+    const submitButton = (event) =>{
+        event.preventDefault();
+        console.log(JSON.stringify(signupField));
+       // alert(signupField.first_name);
+        fetch('users.php', {
+            method: 'POST',
+            headers:"Content-Type: application/json",
+            body: JSON.stringify(signupField)
+          })
+          .then(response => response.text());
+    }
+
+
     return (
         <div className='parentDiv'>
             <div className='login-green'>
                 <h1>habitable.</h1>
                 <h5>Life's busy. We get it and we got you.</h5>
             </div>
-            <div className='login-white'>
+            <form className='login-white'  name="form" onSubmit={submitButton}>
                 <h1>Sign Up</h1>
-                <input type='text' placeholder='Username'></input>
-                <input type='password' placeholder='Password'></input>
-                <input type='email' placeholder='Email'></input>
-                <input type='text' placeholder='First Name'></input>
-                <input type='text' placeholder='Last Name'></input>
-                <input type='date' placeholder='DOB'></input>
-                <Button variant="success">Sign Up</Button>
+                <input type='text' name="username" onChange={inputsHandler} placeholder='Username' value={signupField.username}></input>
+                <input type='password' name="password" onChange={inputsHandler} placeholder='Password' value={signupField.password}></input>
+                <input type='email' name="email" onChange={inputsHandler} placeholder='Email' value={signupField.email}></input>
+                <input type='text' name="first_name" onChange={inputsHandler} placeholder='First Name' value={signupField.first_name}></input>
+                <input type='text' name="last_name" onChange={inputsHandler} placeholder='Last Name' value={signupField.last_name}></input>
+                <input type='date' name="DOB" onChange={inputsHandler} placeholder='DOB' value={signupField.dob}></input>
+                {/* <Button onClick={submitButton} variant="success">Sign Up</Button> */}
+                <input type = "submit" />
                 <div>
                 <span>Already a member? <a onClick={()=>{setNewContext(2)}}>login</a> instead.</span>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
--- users.php
+++ users.php
@@ -1,17 +1,41 @@
 <?php
+// Include the main Propel script
+require_once './vendor/propel/runtime/lib/Propel.php';
+
+// Initialize Propel with the runtime configuration
+Propel::init("./build/conf/habitable-conf.php");
+
+// Add the generated 'classes' directory to the include path
+set_include_path("./build/classes" . PATH_SEPARATOR . get_include_path());
+
+include "autoload.php";
+
+$postObj=json_decode(file_get_contents("php://input"));
+//some code
+// error_log('your jason is ' . json_encode($postObj) . ' and your name is ' . $postObj->{'username'});
+
+// $STRING = implode(", ", $_REQUEST);
+// error_log("\n your data is " . $STRING . "\n", 0);
+
+// $data = json_decode($_POST['body']);
+if("" != trim($postObj->{'username'})){
+   
+     $user = new Users();
+     $user->setUsername( $postObj->{'username'});
+     $user->setPassword( $postObj->{'password'});
+     $user->setEmail( $postObj->{'email'});
+     $user->setFirstname( $postObj->{'first_name'});
+     $user->setLastname( $postObj->{'last_name'});
+     $user->setDob(date('d-m-y', strtotime('11-12-21')));
+     $user->setDatejoined(date('d-m-y', strtotime('11-12-21')));
+     $user->setLastlogin(date('d-m-y', strtotime('11-12-21')));
+     $user->setApikey('sefgsef');
+     $user->save();
+    
+    echo $user->getUsername();
+}   
 
 
-function updateUser(string $username, string $passcode, string $email, string $firstname, string $lastname, string $api) {
-    $conn = new mysqli(env('servername'), env('username'), env('password'), env('dbname'));
-   $sql = "INSERT INTO 
-    Users (username, passcode, email, firstname, lastname, apikey) 
-    VALUES ('$username', '$passcode', '$email', '$firstname', '$lastname',  '$api')";
-    if ($conn->query($sql) === TRUE) {
-        echo "Users updated successfully";
-      } else {
-        echo "Error updating table: " . $conn->error;
-      }
-}
 
 
 ?>
